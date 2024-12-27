a9b9267cc0c642b0583276e680d88d592219fa48
Koffi Kittleson <koffi.k.collins@gmail.com>
2024-12-27 09:18:05 -0600

    refactored routing for better code style and structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 // import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router";
 import CardDragDrop from './components/CardDragDrop.tsx' 
+import Navigation from "./components/Navigation.tsx"; 
+
 
 
 function App() {
 
   return (
     <>
-     <p>hi</p>
-     < CardDragDrop/>
+      <BrowserRouter>
+        <Navigation/>
+          <Routes>
+            <Route path="/" element={<p>hi</p>} />
+            <Route path="/Draft" element={< CardDragDrop/>} />
+          </Routes>
+        </BrowserRouter>
     </>
   )
 }
 
-export default App
+export default App 
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,26 @@
-// import {
-//     type RouteConfig,
-//     route,
-//   } from "@react-router/dev/routes";
-  
-//   export default [
-//     route("some/path", "./some/file.tsx"),
-//     // pattern ^           ^ module file
-//   ] satisfies RouteConfig;
-  
+import { NavLink, Link } from "react-router";
+
+
+export default function Navigation() {
+    return (
+        <nav>
+      {/* NavLink makes it easy to show active states */}
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          isActive ? "active" : ""
+        }
+      >
+        Home
+      </NavLink>
+
+      <Link to="draft">Draft!</Link>
+      <Link to="about">About Us</Link>
+      <Link to="collection">Vault</Link>
+    </nav>
+    // <>
+    // im a nav bar
+    // </>
+    )
+}
+
